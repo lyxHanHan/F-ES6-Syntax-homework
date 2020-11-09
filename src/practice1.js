70329bb53b318837d594bbd6b7f33d478b6fb27f
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-10 00:49:15 +0800

    complete practice1 and practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = ({ data, column }) => {
+    let person = [];
+    data.map(item => {
+        let describle = {};
+        item.forEach((infor,index) => {
+            describle[column[index].name] = item[index];
+        });
+        person.push(describle);
+    })
+    return person;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
+const inject = (item,section) => {
+    let length=item.length;
+
+    section.map(element => {
+        let section=element.content;
+        let index = element.index;
+        if(index > length){
+            item.splice(index+1,0,section)
+        }else{
+            item.splice(index,0,section)
+        }
+        length=index;
+    })
+
+    return item
+}
 export { inject };
+
+
+// array.splice(index,howmany,item1,.....,itemX)
